271fae18b59a4c7f76afdb29a6cb2843a0db41f2
Berlimor <eldaryusufovooo@gmail.com>
2023-05-05 17:42:01 +0300

    More minor improvements.

--- src/schemas/field.py
+++ src/schemas/field.py
@@ -2,26 +2,24 @@ from pydantic import BaseModel
 from typing import Any
 from enum import Enum
 
-from src.controllers.controller import Unit
-
-class Unit(BaseModel):
+class Unit(BaseModel): #Надо понять, как относятся юнит из контроллера и этот юнит
     unit_type: str
 
 class UnitRetrieve(BaseModel):
-    pass
+    unit: Unit
 
 class Cell(BaseModel):
     terrain_type: str
     unit: dict | None
     build_type: str | None
 
-class CellRetrieve(BaseModel):
-    pass
+class CellRetrieve(BaseModel): #Доделать ретривы, с учётом фронта
+    cell: Cell
 
-class CellTerrainType(str, Enum):
+class CellTerrainType(str, Enum): #Не уверен насчёт правильности
     empty_field = 'empty_field'
-    woods = 'woods'
-    
+    water = 'water'
+    rock = 'rock'
 
 class FieldRetrieve(BaseModel):
     cells: list[list[Any]]
